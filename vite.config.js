471151edab129e4299dfad313ab2208a100308e3
Sonai Barua <sonaiClayMind@gmail.com>
2024-07-29 13:05:11 +0530

    created

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,30 @@
-
 import path from 'path';
 import react from '@vitejs/plugin-react';
 
-
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './dist',);
-export default async () => {
+const BUILD_DIR = path.resolve(__dirname, './dist'); 
 
-  return  {
-    plugins: [
-      react(),
-
-    ],
-    root: SRC_DIR,
-    base: '',
-    publicDir: PUBLIC_DIR,
-    build: {
-      outDir: BUILD_DIR,
-      assetsInlineLimit: 0,
-      emptyOutDir: true,
-      rollupOptions: {
-        treeshake: false,
-      },
+export default {
+  plugins: [react()],
+  root: SRC_DIR,
+  base: '/',
+  publicDir: PUBLIC_DIR,
+  build: {
+    outDir: BUILD_DIR, 
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    rollupOptions: {
+      treeshake: false,
     },
-    resolve: {
-      alias: {
-        '@': SRC_DIR,
-      },
+  },
+  resolve: {
+    alias: {
+      '@': SRC_DIR,
     },
-    server: {
-      host: true,
-    },
-
-  };
-}
+  },
+  server: {
+    host: true,
+    port: 3000, 
+  },
+};
