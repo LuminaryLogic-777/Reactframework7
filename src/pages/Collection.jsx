c71249ffddfc39521d1be089b47382006af0d5d8
Sonai Barua <sonaiClayMind@gmail.com>
2024-07-30 11:21:59 +0530

    added grid

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,25 +1,19 @@
-import React from "react";
+import React from 'react';
 import {
   Page,
   Navbar,
-  NavLeft,
-  NavTitle,
-  NavRight,
-  Toolbar,
-  Block,
   BlockTitle,
-  List,
-  ListItem,
+  Block,
   Button,
-  Link, // Import Link component
-} from "framework7-react";
+  Toolbar
+} from 'framework7-react';
 
 const CollectionPage = () => {
   return (
-    <Page>
-      {/* Navbar with title and menu button */}
+    <Page className="grid-demo">
+      {/* Navbar with title */}
       <Navbar backLink="Back">
-        <NavTitle>Collection</NavTitle>
+        <div className="navbar-title">Collection</div>
       </Navbar>
 
       {/* Main Content */}
@@ -29,50 +23,92 @@ const CollectionPage = () => {
         </BlockTitle>
         <Block>
           <div className="search-filter-container">
-          <form className="search-form">
-            <div className="search-input-wrapper">
-              <i className="material-icons search-icon"></i>
-              <input
-                type="search"
-                placeholder="Search..."
-                className="search-input"
-              />
-            </div>
-          </form>
-          <Button
-            className="filter-button"
-            iconMd="material:filter_list" // You can use any filter icon you prefer
-            onClick={() => document.querySelector('.dropdown-sheet').f7Component.open()}
-          >
-            Filter
-          </Button>
-        </div>
-          <List>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <ListItem
-                key={index}
-                title={`Item ${index + 1}`}
-                subtitle="One line description"
-                mediaItem
-                media={
-                  <img
-                    src={`path_to_image_${index + 1}.jpg`} // Replace with actual image paths
-                    alt="Item"
-                    style={{ width: "80px", height: "80px" }}
-                  />
-                }
-                footer={
-                  <div
-                    style={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    <Button icon="f7:heart_fill" />
-                    <Button icon="f7:arrow_right_circle_fill" />
-                    <Button icon="f7:pin_fill" />
-                  </div>
-                }
-              />
-            ))}
-          </List>
+            <form className="search-form">
+              <div className="search-input-wrapper">
+                <i className="material-icons search-icon"></i>
+                <input
+                  type="search"
+                  placeholder="Search..."
+                  className="search-input"
+                />
+              </div>
+            </form>
+            <Button
+              className="filter-button"
+              iconMd="material:filter_list"
+              onClick={() => document.querySelector('.dropdown-sheet').f7Component.open()}
+            >
+              Filter
+            </Button>
+          </div>
+        </Block>
+
+        {/* Grid Layout */}
+        <BlockTitle>Grid Layout</BlockTitle>
+        <Block>
+          <div className="grid grid-cols-2 grid-gap">
+            <div className="grid-item">2 cols</div>
+            <div className="grid-item">2 cols</div>
+          </div>
+          <div className="grid grid-cols-4 grid-gap">
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+          </div>
+          <div className="grid grid-cols-3 grid-gap">
+            <div className="grid-item">3 cols</div>
+            <div className="grid-item">3 cols</div>
+            <div className="grid-item">3 cols</div>
+          </div>
+          <div className="grid grid-cols-5 grid-gap">
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+          </div>
+        </Block>
+
+        <BlockTitle>No Gap between Columns</BlockTitle>
+        <Block>
+          <div className="grid grid-cols-2">
+            <div className="grid-item">2 cols</div>
+            <div className="grid-item">2 cols</div>
+          </div>
+          <div className="grid grid-cols-4">
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+            <div className="grid-item">4 cols</div>
+          </div>
+          <div className="grid grid-cols-3">
+            <div className="grid-item">3 cols</div>
+            <div className="grid-item">3 cols</div>
+            <div className="grid-item">3 cols</div>
+          </div>
+          <div className="grid grid-cols-5">
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+            <div className="grid-item">5 cols</div>
+          </div>
+        </Block>
+
+        <BlockTitle>Responsive Grid</BlockTitle>
+        <Block>
+          <p>Grid cells have different sizes on Phone/Tablet</p>
+          <div className="grid grid-cols-1 medium-grid-cols-2 grid-gap">
+            <div className="grid-item">1 col / medium 2 cols</div>
+            <div className="grid-item">1 col / medium 2 cols</div>
+          </div>
+          <div className="grid grid-cols-2 medium-grid-cols-4 grid-gap">
+            <div className="grid-item">2 col / medium 4 cols</div>
+            <div className="grid-item">2 col / medium 4 cols</div>
+            <div className="grid-item">2 col / medium 4 cols</div>
+            <div className="grid-item">2 col / medium 4 cols</div>
+          </div>
         </Block>
       </div>
 
