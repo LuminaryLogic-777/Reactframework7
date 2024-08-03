88dbd814eab0a71d747c88a0eafbe83ac5bdd421
Sonaiofficial <sonaibaruaofficial@gmail.com>
2024-08-03 10:10:20 +0530

    added a image into grid

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 import {
   Page,
   Navbar,
+  NavTitle,
+  Toolbar,
   BlockTitle,
-  Block,
   Button,
-  Toolbar
-} from 'framework7-react';
+} from "framework7-react";
+
 
 const CollectionPage = () => {
   return (
-    <Page className="grid-demo">
-      {/* Navbar with title */}
+    <Page>
+      {/* Navbar with title and menu button */}
       <Navbar backLink="Back">
-        <div className="navbar-title">Collection</div>
+        <NavTitle>Collection</NavTitle>
       </Navbar>
 
       {/* Main Content */}
@@ -21,95 +22,51 @@ const CollectionPage = () => {
         <BlockTitle className="block-title block-title-large">
           Collection
         </BlockTitle>
-        <Block>
-          <div className="search-filter-container">
-            <form className="search-form">
-              <div className="search-input-wrapper">
-                <i className="material-icons search-icon"></i>
-                <input
-                  type="search"
-                  placeholder="Search..."
-                  className="search-input"
-                />
-              </div>
-            </form>
-            <Button
-              className="filter-button"
-              iconMd="material:filter_list"
-              onClick={() => document.querySelector('.dropdown-sheet').f7Component.open()}
-            >
-              Filter
-            </Button>
-          </div>
-        </Block>
 
-        {/* Grid Layout */}
-        <BlockTitle>Grid Layout</BlockTitle>
-        <Block>
-          <div className="grid grid-cols-2 grid-gap">
-            <div className="grid-item">2 cols</div>
-            <div className="grid-item">2 cols</div>
-          </div>
+        <div className="GridRow amajan-list">
           <div className="grid grid-cols-4 grid-gap">
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-          </div>
-          <div className="grid grid-cols-3 grid-gap">
-            <div className="grid-item">3 cols</div>
-            <div className="grid-item">3 cols</div>
-            <div className="grid-item">3 cols</div>
-          </div>
-          <div className="grid grid-cols-5 grid-gap">
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-          </div>
-        </Block>
+            {/* Repeated Grid Items */}
+            {[...Array(9)].map((_, index) => (
+              <div className="grid-item" key={index}>
+                <div className="user-logo">
+                  <span>Arijit janaaa</span>
+                </div>
+                <div className="pro-image">
+                  {/* Add the image here */}
+                  <img
+                    src="./images/logo512.png"
+                    alt="Description of the image"
+                    className="image-class"
+                  />
+                </div>
 
-        <BlockTitle>No Gap between Columns</BlockTitle>
-        <Block>
-          <div className="grid grid-cols-2">
-            <div className="grid-item">2 cols</div>
-            <div className="grid-item">2 cols</div>
+                <div className="GridRow">
+                  <span className="badge color-red">5</span>
+                  <span className="badge color-red">5</span>
+                  <span className="badge color-red">5</span>
+                  <span className="badge color-red">5</span>
+                  <span className="badge color-red">5</span>
+                </div>
+                <div className="pro-discription">
+                  Birds Chirping in Space 360°Birds Chirping in Space
+                  360°Birds Chirping in Space 360°Birds Chirping in Space
+                  360°Birds Chirping in Space 360°Birds Chirping
+                </div>
+                <div className="GridRow card-footer-link">
+                  <a href="#" className="button">
+                    1
+                  </a>
+                  <a href="#" className="button">
+                    2
+                  </a>
+                  <a href="#" className="button">
+                    3
+                  </a>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="grid grid-cols-4">
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-            <div className="grid-item">4 cols</div>
-          </div>
-          <div className="grid grid-cols-3">
-            <div className="grid-item">3 cols</div>
-            <div className="grid-item">3 cols</div>
-            <div className="grid-item">3 cols</div>
-          </div>
-          <div className="grid grid-cols-5">
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-            <div className="grid-item">5 cols</div>
-          </div>
-        </Block>
-
-        <BlockTitle>Responsive Grid</BlockTitle>
-        <Block>
-          <p>Grid cells have different sizes on Phone/Tablet</p>
-          <div className="grid grid-cols-1 medium-grid-cols-2 grid-gap">
-            <div className="grid-item">1 col / medium 2 cols</div>
-            <div className="grid-item">1 col / medium 2 cols</div>
-          </div>
-          <div className="grid grid-cols-2 medium-grid-cols-4 grid-gap">
-            <div className="grid-item">2 col / medium 4 cols</div>
-            <div className="grid-item">2 col / medium 4 cols</div>
-            <div className="grid-item">2 col / medium 4 cols</div>
-            <div className="grid-item">2 col / medium 4 cols</div>
-          </div>
-        </Block>
+        </div>
       </div>
 
       {/* Toolbar */}
